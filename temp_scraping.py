0b184a9b481fc2ebb29ee3a66006fc0885971eb9
acardoco <andres.cardosoc12@gmail.com>
2019-04-08 21:43:23 +0200

    -web scraping de precipitacion (l/m^2) hecho desde 2008 para zaragoza

--- temp_scraping.py
+++ temp_scraping.py
@@ -11,7 +11,18 @@ import time
 datos_externos_origen = 'C:/Users/Andrés/Documents/AGUATHON_AEMET/'
 datos_externos_destino = 'datos_externos/procesados/'
 path_datos_antiguos = 'https://datosclima.es/Aemethistorico/Lluviasol.php'
-def aemet_to_csv():
+#------------------------------------------------------
+#------------------------------------------------------
+def tratar_fecha(fecha):
+    #02-01-2008
+    dia = fecha[:2]
+    mes = fecha[3:5]
+    año = fecha[6:10]
+
+    return año + mes + dia
+#------------------------------------------------------
+#------------------------------------------------------
+def aemet_csvs_to_lista():
 
     lista = []
     for mes in os.listdir(datos_externos_origen):
@@ -27,6 +38,9 @@ def aemet_to_csv():
                 df = df.fillna(0)
 
                 zrg = df[df['Estación'] == 'Zaragoza, Canal']
+                if zrg.empty:
+                    zrg = df[df['Estación'] == 'Zaragoza, Valdespartera']
+
                 prec = zrg['Precipitación 00-24h (mm)'].values
                 prec = prec[0]
                 lista.append((fecha, prec))
@@ -35,9 +49,8 @@ def aemet_to_csv():
                 print('error de mierda en el dia ' + dia)
                 lista.append((fecha, 0.0))
 
-    lista = np.asarray(lista)
-    datos_a_exportar = pd.DataFrame(lista, columns=['fecha', 'precipitacion'])
-    datos_a_exportar.to_csv(datos_externos_destino + 'datos_externos.csv', index=False)
+    return lista
+
 #------------------------------------------------------
 #------------------------------------------------------
 def aemet_observatorios():
@@ -49,26 +62,28 @@ def aemet_observatorios():
     # el navegador
     browser = webdriver.Chrome("driver/chromedriver.exe", chrome_options=chrome_options)
     browser.get(path_datos_antiguos)
+    browser.maximize_window()
 
     celda_ciudad = WebDriverWait(browser, 5).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="col2"]/div[1]/div/form/select[1]')))
     celda_ciudad.click()
 
-    time.sleep(2)
+    time.sleep(1)
 
     zrg_celda = WebDriverWait(browser, 5).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="col2"]/div[1]/div/form/select[1]/option[53]')))
     zrg_celda.click()
 
-    time.sleep(2)
+    time.sleep(1)
 
     celda_estacion = browser.find_element_by_xpath('//*[@id="col2"]/div[1]/div/form/select[2]')
     celda_estacion.click()
-    time.sleep(2)
-    zrg_est_celda = browser.find_element_by_xpath('//*[@id="col2"]/div[1]/div/form/select[2]/option[6]')
+    time.sleep(1)
+    #aeropuerto
+    zrg_est_celda = browser.find_element_by_xpath('//*[@id="col2"]/div[1]/div/form/select[2]/option[5]')
     zrg_est_celda.click()
 
-    time.sleep(2)
+    time.sleep(1)
 
     # rellenamos
     #fecha inicial
@@ -89,16 +104,110 @@ def aemet_observatorios():
     boton_buscar = browser.find_element_by_xpath('//*[@id="col2"]/div[1]/div/form/input[5]')
     boton_buscar.click()
 
-    time.sleep(3)
+    time.sleep(1)
 
     # leemos dias
-    # TODO
+    tablas_totales = 6 * 3
+    cuerpo_tabla_xpath = '//*[@id="col2"]/div/div/'
+    medio_tabla_xpath = 'table/tbody/tr/td['
+    fin_tabla_xpath = ']/table/tbody/tr'
+    fin_tabla1_xpath = ']/div/table/tbody/tr'
+
+    indice_tabla = 1
+
+    lista = []
+
+    for tabla in range(0, tablas_totales):
+
+        if indice_tabla > 3:
+            indice_tabla = 1
+
+        print('Indice:', indice_tabla)
+
+        if indice_tabla == 1:
+            tabla_xpath = cuerpo_tabla_xpath + medio_tabla_xpath + '1' + fin_tabla1_xpath
+        else:
+            tabla_xpath = cuerpo_tabla_xpath + medio_tabla_xpath + str(indice_tabla) + fin_tabla_xpath
+        print(tabla_xpath)
+
+        #tabla 1
+        lista_elementos_tabla = browser.find_elements_by_xpath(tabla_xpath)
+
+        idx_elements_lista = 0
+        for e_tabla1 in lista_elementos_tabla:
+            datos_elemento = e_tabla1.text
+            fecha = datos_elemento.split(' ')[0]
+            fecha = tratar_fecha(fecha)
+            precipitacion = datos_elemento.split(' ')[1]
+            if precipitacion == 'Ip':
+                precipitacion = '0.0'
+
+            if idx_elements_lista >0 :
+                lista.append((fecha, precipitacion))
+
+            idx_elements_lista +=1
+
+        if indice_tabla == 3:
+            cuerpo_tabla_xpath = cuerpo_tabla_xpath + 'div[2]/'
+
+        indice_tabla +=1
+
+        # tabla2
+        #//*[@id="col2"]/div/div[1]/table[2]/tbody/tr/td[1]/div/table/tbody/tr[2]/td[2]
+
+        # tabla3
+
+        #//*[@id="col2"]/div/div/table[2]/tbody/tr/td[1]/div/table/tbody/tr
+        #//*[@id="col2"]/div/div/table[2]/tbody/tr/td[2]/table/tbody
+        #//*[@id="col2"]/div/div/table[2]/tbody/tr/td[3]/table/tbody
+
+        #2009
+        #//*[@id="col2"]/div/div/div[2]/table/tbody/tr/td[1]/div/table/tbody
+        #//*[@id="col2"]/div/div/div[2]/table/tbody/tr/td[2]/table/tbody
+        #//*[@id="col2"]/div/div/div[2]/table/tbody/tr/td[3]/table/tbody
+
+        #2010
+        #//*[@id="col2"]/div/div/div[2]/div[2]/table/tbody/tr/td[1]/div/table/tbody
+        #//*[@id="col2"]/div/div/div[2]/div[2]/table/tbody/tr/td[2]/table/tbody
+        #//*[@id="col2"]/div/div/div[2]/div[2]/table/tbody/tr/td[3]/table/tbody
+
+        #2011
+        #//*[@id="col2"]/div/div/div[2]/div[2]/div[2]/table/tbody/tr/td[1]/div/table/tbody
+        #//*[@id="col2"]/div/div/div[2]/div[2]/div[2]/table/tbody/tr/td[2]/table/tbody
+        #//*[@id="col2"]/div/div/div[2]/div[2]/div[2]/table/tbody/tr/td[3]/table/tbody
+
+        #2012
+        #//*[@id="col2"]/div/div/div[2]/div[2]/div[2]/div[2]/table/tbody/tr/td[1]/div/table/tbody
+        #//*[@id="col2"]/div/div/div[2]/div[2]/div[2]/div[2]/table/tbody/tr/td[2]/table/tbody
+        #//*[@id="col2"]/div/div/div[2]/div[2]/div[2]/div[2]/table/tbody/tr/td[3]/table/tbody
+
+        #2013
+        #//*[@id="col2"]/div/div/div[2]/div[2]/div[2]/div[2]/div[2]/table/tbody/tr/td[1]/div/table/tbody
+        #//*[@id="col2"]/div/div/div[2]/div[2]/div[2]/div[2]/div[2]/table/tbody/tr/td[2]/table/tbody
+        #//*[@id="col2"]/div/div/div[2]/div[2]/div[2]/div[2]/div[2]/table/tbody/tr/td[3]/table
+
+    print(lista[-5:])
+    print(len(lista))
 
     browser.close()
 
+    return lista
 
+#------------------------------------------------------
+#------------------------------------------------------
+#------------------------------------------------------
+def aemet_to_csv():
 
+    lista = []
+
+    lista_obs = aemet_observatorios()
+    lista_csvs = aemet_csvs_to_lista()
+    lista = lista_obs + lista_csvs
+
+    lista = np.asarray(lista)
+    datos_a_exportar = pd.DataFrame(lista, columns=['fecha', 'precipitacion'])
+    datos_a_exportar.to_csv(datos_externos_destino + 'datos_externos.csv', index=False)
 #------------------------------------------------------
 #------------------------------------------------------
 #------------------------------------------------------
-aemet_observatorios()
+aemet_to_csv()
